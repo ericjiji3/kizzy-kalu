f4389c363f824e2589addd3c0ea2456bde8629f7
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2025-01-21 19:31:28 -0500

    fixing animatiom on mobile

--- src/app/components/Slideshow/Slideshow.js
+++ src/app/components/Slideshow/Slideshow.js
@@ -187,7 +187,7 @@ export default function Slideshow(){
                     />
                 </div>
                 <div className={imageIndex == 1 ? 'carousel-images-container active' : 'carousel-images-container'} project-index={0} slide-index={1}>
-                    <video width="320" height="240" controls playsInline
+                    <video width="320" height="240" controls playsInline preload="metadata"
                         className='carousel-image img-1-2-1'
                     >
                         <source src="https://videos.ctfassets.net/tu2om6uq2183/6ZfqaKTcs2BowZB91sACj3/b0663bf5fc57edec85b36d48bb41570c/CAR_SCENE_FINAL_2_-9X16-__1_.mp4" type="video/mp4" />
@@ -209,7 +209,7 @@ export default function Slideshow(){
                         height={1000} 
                         className='carousel-image img-1-3-1'
                     />
-                    <video width="320" height="240" controls playsInline
+                    <video width="320" height="240" controls playsInline preload="metadata"
                         className='carousel-image img-1-3-2'
                     >
                         <source src="https://videos.ctfassets.net/tu2om6uq2183/273QsF5eZJv2lmCJtqYJjJ/07ee4986762cb333f38343cba3069934/CHASE_SCENE_FINAL_-9x16-.mov" type="video/mp4" />
@@ -224,25 +224,25 @@ export default function Slideshow(){
                     />
                 </div>
                 <div className={imageIndex == 3 ? 'carousel-images-container project-1-4 active' : 'carousel-images-container'} project-index={0} slide-index={3}>
-                    <video width="320" height="500" controls playsInline
+                    <video width="320" height="500" controls playsInline preload="metadata"
                         className='carousel-image img-1-4-1'
                     >
                         <source src="https://videos.ctfassets.net/tu2om6uq2183/7kDBq8Png4A7hUMJfs6Ywn/12a07beb78fd45a5311d94830b8c3278/FIGHTER5X4_KHRIS.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
-                    <video width="320" height="500" controls playsInline
+                    <video width="320" height="500" controls playsInline preload="metadata"
                         className='carousel-image img-1-4-2'
                     >
                         <source src="https://videos.ctfassets.net/tu2om6uq2183/60gTP7KzimbNzDZ6nFGaLt/633114a88190f35292038772a1002c90/FIGHTER5X4_MANNY.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
-                    <video width="320" height="500" controls playsInline
+                    <video width="320" height="500" controls playsInline preload="metadata"
                         className='carousel-image img-1-4-3'
                     >
                         <source src="https://videos.ctfassets.net/tu2om6uq2183/k5aj1RlxKlp6yJtArE5wq/be045eb2b39ef44ad6447ebd1331ddc7/FIGHTER5X4_MATT.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
-                    <video width="320" height="500" controls playsInline
+                    <video width="320" height="500" controls playsInline preload="metadata"
                         className='carousel-image img-1-4-4'
                     >
                         <source src="https://videos.ctfassets.net/tu2om6uq2183/1VIboL3tkUBNiJWvdomBuh/5dff31a6b1fe0022eb40e06d6409adfe/FIGHTER5X4_MELL.mp4" type="video/mp4" />
